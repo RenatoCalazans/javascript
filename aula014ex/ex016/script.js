477746344bf8e788caa175464920b9e32bdb1d73
RenatoCalazans <148402259+RenatoCalazans@users.noreply.github.com>
2024-04-18 18:14:40 -0300

    contator

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -6,15 +6,22 @@ function contar(){
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
         window.alert('[erro]falta dados!')
+        res.innerHTML= 'impossivel contar'
     }else{
         res.innerHTML ='contando: '
         var i =Number(ini.value)
         var f =Number(fim.value)
         var p =Number(passo.value)
-
+        if(p <=0){
+            window.alert('passo invalido')
+        }
         if(i < f){
             for(var c = i; c <= f; c += p){
                 res.innerHTML += `\u{1F64F} ${c} `
+            }
+        }else{
+            for(c =i; c >=f; c-= p){
+                res.innerHTML += `\u{1F64F} ${c}  `
             }
         }
 

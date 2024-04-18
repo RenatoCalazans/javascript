d4e45af713f1555e074d77684e0d0718153f109d
RenatoCalazans <148402259+RenatoCalazans@users.noreply.github.com>
2024-04-18 18:02:59 -0300

    contador

---
+++ aula014ex/ex016/script.js
@@ -0,0 +1,23 @@
+function contar(){
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.getElementById('txtf')
+    var passo = window.document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+        window.alert('[erro]falta dados!')
+    }else{
+        res.innerHTML ='contando: '
+        var i =Number(ini.value)
+        var f =Number(fim.value)
+        var p =Number(passo.value)
+
+        if(i < f){
+            for(var c = i; c <= f; c += p){
+                res.innerHTML += `\u{1F64F} ${c} `
+            }
+        }
+
+     
+    }
+}

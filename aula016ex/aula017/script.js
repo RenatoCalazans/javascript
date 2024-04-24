f85368c1bdd1713f932a0217ecbcd45746602e0c
RenatoCalazans <148402259+RenatoCalazans@users.noreply.github.com>
2024-04-24 20:16:19 -0300

    analize de valores

---
+++ aula016ex/aula017/script.js
@@ -0,0 +1,26 @@
+var num = document.querySelector('input#fnum')
+var lista =document.querySelector('select#flista')
+var res = document.querySelector('div#res')
+var valores = []
+
+function isnumero(n) {
+    if(Number(n) >=1 && Number(n) <=100 ){
+        return true
+    }else{
+        return false
+    }
+}
+function inlista(n, l) {
+    if(l.indexOf(Number(n)) != -1){
+        return true
+    }else {
+        return false
+    }
+}
+function adicionar(){
+    if(isnumero(num.value) && !inlista(num.value , valores)){
+       
+    }else{
+        window.alert('valor invalido ou ja encontrado na lista.')
+    }
+}

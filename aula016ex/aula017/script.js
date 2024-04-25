248fee70c393b8500e8749be085082887c79f8f9
RenatoCalazans <148402259+RenatoCalazans@users.noreply.github.com>
2024-04-25 20:47:29 -0300

    exercicio

--- aula016ex/aula017/script.js
+++ aula016ex/aula017/script.js
@@ -19,8 +19,39 @@ function inlista(n, l) {
 }
 function adicionar(){
     if(isnumero(num.value) && !inlista(num.value , valores)){
-       
+       valores.push(Number(num.value))
+       var item = document.createElement('option')
+       item.text= `valor ${num.value} adicionado`
+       lista.appendChild(item)
+       res.innerHTML =""
     }else{
         window.alert('valor invalido ou ja encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
+
+function finalizar(){
+    if(valores.length == 0){
+       window.alert('adicione valores antes de finalizar')
+    }else{
+        var tot = valores.length
+        var maior =valores[0]
+        var menor =valores[0]
+        var soma =0
+        var media = 0
+        for(var pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+            menor =valores[pos]
+         }
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> ao todo, temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p>o maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>o menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>somando todos os valores,temos ${soma}</p>`
+    }
